4da8d41cb21a9e7803200eeff24f21caf7fdedb7
jasna <jasnathevarakandi@gmail.com>
2025-04-16 06:18:28 +0400

    component host

--- client/src/Layout/RootLayout.jsx
+++ client/src/Layout/RootLayout.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Header } from "../components/user/Header";
-import { Footer } from "../components/user/Footer";
 import { Outlet, useLocation } from "react-router-dom";
-import { UserHeader } from "../components/user/UserHeader";
 import { useDispatch, useSelector } from "react-redux";
 import { axiosInstance } from "../config/axiosInstance";
 import { clearUser, saveUser } from "../redux/features/userSlice";
+import { Header } from "../Components/user/Header";
+import { Footer } from "../Components/user/Footer";
+import { UserHeader } from "../Components/user/UserHeader";
 
 export const RootLayout = () => {
     const user = useSelector((state) => state.user);
@@ -34,7 +34,7 @@ export const RootLayout = () => {
 
     return isLoading ? null : (
         <div>
-            {user.isUserAuth ? <UserHeader /> : <Header />}
+            {user.isUserAuth ? <UserHeader/> : <Header/>}
             <div className="min-h-96">
                 <Outlet />
             </div>
