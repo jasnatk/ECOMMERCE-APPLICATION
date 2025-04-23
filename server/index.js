3ab0b54962e9ff48b3d2d20e11df9eec232ade8f
jasna <jasnathevarakandi@gmail.com>
2025-04-23 04:55:29 +0400

    options s

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
       origin: ["http://localhost:5173","https://ecommerce-application-clientview1.vercel.app"],
       credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTION"]
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
   })
 );
 
