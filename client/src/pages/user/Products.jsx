5ca24d88adec3523ddd0e825cd9abd6db0fa00e8
jasna <jasnathevarakandi@gmail.com>
2025-04-23 05:59:22 +0400

    viewbox

--- client/src/pages/user/Products.jsx
+++ client/src/pages/user/Products.jsx
@@ -122,7 +122,7 @@ export const Product = () => {
           className="w-5 h-5"
           fill="none"
           stroke="currentColor"
-          viewBox="0 24 24"
+          viewBox="0 0 24 24"
         >
           <path
             strokeLinecap="round"
